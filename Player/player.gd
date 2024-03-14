da8272fdc0c1ce793a26939b41f163540dfc210b
Kulkarni <Amrut.Kulkarni@xylem.com>
2024-03-13 21:18:00 -0400

    Player controller WIP

--- Player/player.gd
+++ Player/player.gd
@@ -1,7 +1,38 @@
-extends RigidBody3D
+extends CharacterBody3D
+
+
+@export var speed = 5.0
+@export var JUMP_VELOCITY = 4.5
+@export var sensitivity = 0.3
+
+@onready var head = $Head
+@onready var camera = $Head/Camera
+@onready var weapon_mount = $WeaponMount
+@onready var gun_base = $WeaponMount/GunBase
+@export var weapon: Node
+@export var bullet: Node
+
+# Get the gravity from the project settings to be synced with RigidBody nodes.
+var gravity = ProjectSettings.get_setting("physics/3d/default_gravity")
 
 func _ready():
 	pass
-	
+
 func _physics_process(delta):
-	pass
+	# Add the gravity.
+	if not is_on_floor():
+		velocity.y -= gravity * delta
+		
+	if Input.is_action_just_pressed("ui_accept") and is_on_floor():
+		velocity.y = JUMP_VELOCITY
+	
+	var input_dir = Input.get_vector("move_left", "move_right", "move_front", "move_back")
+	var direction = (transform.basis * Vector3(input_dir.x, 0, input_dir.y)).normalized()
+	if direction:
+		velocity.x = direction.x * speed
+		velocity.z = direction.z * speed
+	else:
+		velocity.x = move_toward(velocity.x, 0, speed)
+		velocity.z = move_toward(velocity.z, 0, speed)
+
+	move_and_slide()
