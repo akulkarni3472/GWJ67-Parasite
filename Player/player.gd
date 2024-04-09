d5261ceeddf1e46b864b365d98967d44cc337b5c
Kulkarni <Amrut.Kulkarni@xylem.com>
2024-04-08 23:38:11 -0400

    Update

--- Player/player.gd
+++ Player/player.gd
@@ -9,6 +9,13 @@ extends CharacterBody3D
 @export var min_view_angle = -50
 @export var max_view_angle = 80
 
+@export var bob_freq = 1.2
+@export var bob_amp = 0.04
+@export var t_bob = 0.0
+
+@export var base_fov = 75.0
+@export var fov_change = 1.5
+
 @export var weapon: Node
 @export var bullet: Node
 
@@ -45,17 +52,32 @@ func _physics_process(delta):
 	else:
 		sprint_mult = 1
 		
-	var input_dir = Input.get_vector("move_left", "move_right", "move_front", "move_back")
+	var input_dir = Input.get_vector("leftward", "rightward", "forward", "backward")
 	var direction = (head.transform.basis * Vector3(input_dir.x, 0, input_dir.y)).normalized()
-	if direction:
-		velocity.x = direction.x * speed * sprint_mult
-		velocity.z = direction.z * speed * sprint_mult
+	if is_on_floor():
+		if direction:
+			velocity.x = direction.x * speed
+			velocity.z = direction.z * speed
+		else:
+			velocity.x = lerp(velocity.x, direction.x * speed, delta * 7.0)
+			velocity.z = lerp(velocity.z, direction.z * speed, delta * 7.0)
 	else:
-		velocity.x = move_toward(velocity.x, 0, speed)
-		velocity.z = move_toward(velocity.z, 0, speed)
-
+		velocity.x = lerp(velocity.x, direction.x * speed, delta * 3.0)
+		velocity.z = lerp(velocity.z, direction.z * speed, delta * 3.0)
+		
+	t_bob += delta * velocity.length() * float(is_on_floor())
+	camera.transform.origin = _headbob(t_bob)
+	
+	fire(delta)
+	
 	move_and_slide()
 	
 func fire(delta):
 	if Input.is_action_just_pressed("shoot"):
 		print("Bang!")
+
+func _headbob(time) -> Vector3:
+	var pos = Vector3.ZERO
+	pos.y = sin(time * bob_freq) * bob_amp
+	pos.x - cos(time * bob_freq / 2) * bob_amp
+	return pos
